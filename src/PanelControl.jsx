0a867ded3e59ed5277967a892a6b7fec62e863bf
jhon jairo <jhonjdv2022@outlook.com>
2025-02-25 20:59:53 -0500

    creacion del panel de control con sus estilos

---
+++ src/PanelControl.jsx
@@ -0,0 +1,23 @@
+function PanelControl() {
+  return (
+    <div className="panel-control">
+      <aside className="panel-control-aside">
+        <div>
+          <img src="" alt="" />
+        </div>
+        <nav>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </nav>
+      </aside>
+      <section className="panel-control-section">
+        <h1>Mensaje de bienvenida</h1>
+      </section>
+    </div>
+  );
+}
+export default PanelControl;
